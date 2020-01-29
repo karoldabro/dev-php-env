f45b38b97f49d31e95b0fe41e14fdb2955e5edcb
Karol Dąbrowski <karol_dabrowski@interia.eu>
2020-01-29 21:19:25 +0100

    Update PHPMA

--- services/phpmyadmin/config.user.inc.php
+++ services/phpmyadmin/config.user.inc.php
@@ -4,7 +4,7 @@ $cfg['Servers'] = [
     1 => [
         'auth_type' => 'cookie',
         'host' => 'localhost',
-        'port' => 3306,
+        'port' => getenv('SERVICE_DB_PORT'),
         'verbose' => 'Verbose name 1',
     ],
 ];
